7a70381924a1be80fc62a59a59312f4594b2ebee
chukhnov <nevadch@gmail.com>
2016-02-03 19:44:26 +0200

    add fetch and body-parser

--- client/app.js
+++ client/app.js
@@ -1,56 +1,70 @@
 import React from 'react'
-import { render } from 'react-dom'
-import { browserHistory, Router, Route, Link } from 'react-router'
+import {render} from 'react-dom'
+import {browserHistory, Router, Route, Link} from 'react-router'
+
 
 
 const Register = React.createClass({
     getInitialState: function () {
         return {
             username: " ",
-            password: " "
+            password: " ",
+            showResults: false
         };
     },
-    nameChange: function (event) {
-        this.setState({username: event.target.value});
-    },
-    passChange: function (event) {
-        this.setState({password: event.target.value});
+    onFieldChanged: function (event) {
+        this.setState({[event.target.name]: event.target.value});
     },
 
     render() {
         return (
-            <form>
+            <form >
                 <div>
                     <label>Username:</label>
-                    <input type="text" name="username" onChange={this.nameChange}/><br/>
+                    <input type="text" name="username" onChange={this.onFieldChanged}/>
+                    <br/>
                 </div>
                 <div>
                     <label>Password:</label>
-                    <input type="password" name="password" onChange={this.passChange}/>
+                    <input type="password" name="password" onChange={this.onFieldChanged}/>
                 </div>
                 <div>
-                    <input type="submit" onClick={this._onChange} value="Submit"/>
+                    <button type="button" onClick={this._onChange} value="1">Login</button>
+                    <button type="button" onClick={this._onChange} value="2">Register</button>
+                    { this.state.showResults ? <Dashboard /> : null }
                 </div>
             </form>
+
         )
     },
     _onChange: function (event) {
-        event.preventDefault();
-        var target = this.state.username;
-        var target1 = this.state.password;
-        console.log(target, target1);
-        var xhr = new XMLHttpRequest();
-        var url = "http://127.0.0.1";
-        xhr.open("POST", url, true);
-        xhr.send(target);
-        xhr.onload = function () {
-            var post = xhr.responseText;
-                console.log(post)
-        };
-        xhr.onerror = function () {
-            console.log("Error")
+        var url = " ";
+        if(event.target.value == 1){
+            url = '/api/1/login'
+        }else if(event.target.value == 2){
+            url = '/api/1/register'
         }
-
+        fetch(url, {
+            method: 'post',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: this.state.username,
+                password: this.state.password
+            })
+        })
+    }
+});
+const Dashboard = React.createClass({
+    render() {
+        return (
+            <div>
+                <h1>Dashboard</h1>
+                <p>You made it!</p>
+            </div>
+        )
     }
 });
 
@@ -58,7 +72,8 @@ const Register = React.createClass({
 render((
     <Router history={browserHistory}>
         <Route path="/" component={Register}>
-            <Route path="register" component={Register} />
+            <Route path="/register" component={Register}/>
+            <Route path="/dashboard" component={Dashboard}/>
         </Route>
     </Router>
 ), document.getElementById("application"));
--- server/app.js
+++ server/app.js
@@ -3,6 +3,9 @@ import path from 'path'
 import mongoose from 'mongoose'
 import conig from './modules/application/config/index'
 import {router as ApplicationRouter} from './modules/application/index'
+import multer from 'multer'
+import bodyParser from 'body-parser'
+
 var LocalStrategy = require('passport-local').Strategy;
 var passportLocalMongoose = require('passport-local-mongoose');
 var passport = require('passport');
@@ -10,10 +13,13 @@ var passport = require('passport');
 mongoose.connect('mongodb://localhost/MyDatabase');
 const app = express();
 
+app.use(multer().fields())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.use(express.static(path.resolve(__dirname, '../public')));
 app.use((req, res, next) => {
-    req.path.indexOf('api/1') != -1 ? next() : res.sendFile(path.resolve(__dirname, '../public/index.html'))
+    req.path.indexOf('api/1') != -1 ? next() : res.sendFile(path.resolve(__dirname, '../public/index.html'));
 });
 
 var Schema = mongoose.Schema;
@@ -23,41 +29,39 @@ var UserSchema = new Schema({
 });
 
 UserSchema.plugin(passportLocalMongoose);
-var UserDetails = mongoose.model('User',UserSchema);
+var UserDetails = mongoose.model('User', UserSchema);
 
 passport.use(new LocalStrategy(UserDetails.authenticate()));
 
 passport.serializeUser(UserDetails.serializeUser());
 passport.deserializeUser(UserDetails.deserializeUser());
+app.use(passport.initialize());
+app.use(passport.session());
 
 
-
-
-app.get('/loginFailure' , function(req, res, next){
+app.get('/loginFailure', function (req, res, next) {
     res.send('Failure to authenticate');
 });
 
-app.get('/loginSuccess' , function(req, res, next){
+app.get('/loginSuccess', function (req, res, next) {
     res.send('Successfully authenticated');
 });
 
-app.post('/login',
+app.post('/api/1/login',
     passport.authenticate('local', {
-        successRedirect: '/loginSuccess',
-        failureRedirect: '/loginFailure'
+
     }));
 
 
-
-app.post('/register', function(req, res, next) {
-    UserDetails.register(new UserDetails({username: req.body.username}), req.body.password, function(err) {
+app.post('/api/1/register', function (req, res, next) {
+    console.log(req.body)
+    UserDetails.register(new UserDetails({username: req.body.username}), req.body.password, function (err) {
         if (err) {
             console.log('error while user register!', err);
             return next(err);
         }
         console.log('user registered!');
-        res.end('Strategy');
-        res.redirect('/');
+        res.end('ok');
     });
 });
 
